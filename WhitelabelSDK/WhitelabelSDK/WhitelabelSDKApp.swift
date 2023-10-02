da11bd6d898e5483121b4c6d0f300cf44f192a87
Lika <lika@solarlabs.ee>
2023-10-02 13:09:09 +0300

    Create project

---
+++ WhitelabelSDK/WhitelabelSDK/WhitelabelSDKApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhitelabelSDKApp.swift
+//  WhitelabelSDK
+//
+//  Created by Lika Vorobeva on 02.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct WhitelabelSDKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
