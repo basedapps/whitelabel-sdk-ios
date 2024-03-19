e8b491d031ec41e678d81aab76fe579f9c868530
Lika Vorobeva <lika@solarlabs.ee>
2024-03-19 14:38:54 +0200

    Add background and update app icon

--- WhitelabelSDK/WhitelabelSDK/Root/ViewController.swift
+++ WhitelabelSDK/WhitelabelSDK/Root/ViewController.swift
@@ -34,6 +34,19 @@ extension ViewController {
         webView = WKWebView(frame: .zero, configuration: webConfiguration)
         webView.allowsBackForwardNavigationGestures = true
         webView.scrollView.bounces = false
+        webView.scrollView.backgroundColor = UIColor(red: 0.00, green: 0.02, blue: 0.06, alpha: 1.00)
+        webView.backgroundColor = UIColor(red: 0.00, green: 0.02, blue: 0.06, alpha: 1.00)
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [
+            UIColor(red: 0, green: 6/255, blue: 16/255, alpha: 1).cgColor,
+            UIColor(red: 25/255, green: 31/255, blue: 49/255, alpha: 1).cgColor
+        ]
+        gradientLayer.locations = [0.0, 0.2, 1.0]
+        gradientLayer.frame = view.bounds
+        webView.scrollView.layer.insertSublayer(gradientLayer, at: 0)
+        webView.isOpaque = false
+        
         view = webView
         
         let urlRequest = URLRequest(
